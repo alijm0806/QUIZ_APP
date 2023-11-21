33ea196a0c69a93a14bb5e3b91b2e9383994d6c4
alijm0806 <alijm1104@gmail.com>
2023-11-20 22:04:55 -0600

    Add Header + Quiz components

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const Quiz = () => {
+  const [] = useState();
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  return (
+    <p>
+      Currently active Question
+    </p>
+  )
+}
+
+export default Quiz
